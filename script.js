b7aa139d7ca282ef9e42575bd4cec1e37d1161ce
Kevin Delahunt <dela0130@algonquinlive.com>
2022-11-15 17:15:32 -0500

    Functional, responsive layout completed

---
+++ script.js
@@ -0,0 +1,34 @@
+const listitems = [
+    {item:'Temperance', description:'Eat not to dullness; drink not to elevation.'},
+    {item:'Silence', description:'Speak not but what may benefit others or yourself; avoid trifling conversation.'},
+    {item:'Order', description:'Let all your things have their places; let each part of your business have its time.'},
+    {item:'Resolution', description:'Resolve to perform what you ought; perform without fail what you resolve.'},
+    {item:'Frugality', description:'Make no expense but to do good to others or yourself; i.e., waste nothing.'},
+    {item:'Industry', description:"Lose no time; be always employ'd in something useful; cut off all unnecessary actions."},
+    {item:'Sincerity', description:'Use no hurtful deceit; think innocently and justly, and, if you speak, speak accordingly.'},
+    {item:'Justice', description:'Wrong none by doing injuries, or omitting the benefits that are your duty.'},
+    {item:'Moderation', description:'Avoid extremes; forbear resenting injuries so much as you think they deserve.'},
+    {item:'Cleanliness', description:'Tolerate no uncleanliness in body, cloaths, or habitation.'},
+    {item:'Tranquillity', description:'Be not disturbed at trifles, or at accidents common or unavoidable.'},
+    {item:'Chastity', description:"Rarely use venery but for health or offspring, never to dullness, weakness, or the injury of your own or another's peace or reputation."},
+    {item:'Humility', description:'Imitate Jesus and Socrates.'}
+]
+
+const $list = document.getElementById('list')
+const listhtml = []
+const $main = document.getElementsByName('list-main')
+
+for (const listitem of listitems) {
+    listhtml.push(`
+        <li class="listitem tooltip"> ${listitem.item} <span class="tooltiptext">${listitem.description}</span></li>
+    `)
+
+}
+
+$list.innerHTML = listhtml.join('')
+
+$list.addEventListener ('mouseover', function (e) {
+    if (e.target.classList.contains('listitem')) {
+        console.log(e.target.dataset.description)
+    }
+})
